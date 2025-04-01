d1db415dca7c391af515b08d774d9ace5cc7eb48
miguel angel <miguelangeljimenez1047@gmail.com>
2025-04-01 09:36:43 -0500

    CHORE: minor changes ⚒️

--- src/sections/hero/hero.tsx
+++ src/sections/hero/hero.tsx
@@ -2,7 +2,7 @@ import { AuroraText } from '@/components/ui/aurora-text'
 
 export function Hero() {
   return (
-    <section className='max-w-5xl w-full mx-auto px-4 text-center space-y-2'>
+    <section className='max-w-5xl w-full mx-auto px-4 text-center space-y-2 pb-32'>
       <h1 className='text-5xl font-bold text-balance'>
         Theme Creator for <AuroraText>Shadcn</AuroraText>{' '}
       </h1>
