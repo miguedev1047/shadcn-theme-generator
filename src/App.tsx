d95ccbf2e89d6bd2903266152ebe9f76e3453f95
miguel angel <miguelangeljimenez1047@gmail.com>
2025-04-01 09:35:46 -0500

    FEAT: add hero section 🎨

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Header } from '@/components/header'
+import { Hero } from '@/sections/hero'
 
 function App() {
   return (
     <>
       <Header />
-      <main>
-        
+      <main className='py-12 md:py-24'>
+        <Hero />
       </main>
     </>
   )
---
+++ src/sections/hero/hero.tsx
@@ -0,0 +1,15 @@
+import { AuroraText } from '@/components/ui/aurora-text'
+
+export function Hero() {
+  return (
+    <section className='max-w-5xl w-full mx-auto px-4 text-center space-y-2'>
+      <h1 className='text-5xl font-bold text-balance'>
+        Theme Creator for <AuroraText>Shadcn</AuroraText>{' '}
+      </h1>
+      <p className='text-lg text-muted-foreground'>
+        Create, customize, and export themes for shadcn with support for
+        Tailwind 3 and 4.
+      </p>
+    </section>
+  )
+}
